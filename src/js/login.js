4cb38a42bb257421ea31a1c4cfef7dbbc0a11c3c
Roberto Paternina <paternina.dev@gmail.com>
2021-10-06 21:40:10 -0500

    login v1.0

--- src/js/login.js
+++ src/js/login.js
@@ -3,7 +3,7 @@ const _$ = element => document.querySelector(element)
 const __$ = element => document.querySelectorAll(element)
 // fin helpers
 window.addEventListener('DOMContentLoaded', () => {
-    let loginform = _$('#login-form')    
+    let loginform = _$('#login-form')
     loginform.addEventListener('submit', login)
 })
 
@@ -11,10 +11,10 @@ function login(event) {
     event.preventDefault()
     let username = _$('#username')
     let password = _$('#password')
-    if(username.value === "" || password.value === "") {
+    if (username.value === "" || password.value === "") {
         let error = _$('.error')
         error.innerHTML = "No se pueden enviar valores vacios"
     } else {
-        console.table({"Username":username.value, "Password":password.value})
-    }    
+        window.location.replace('/src/dashboard/index.html')
+    }
 }
